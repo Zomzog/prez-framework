804161067e35456bbc8bac39a69a5d019cd23d40
Zomzog <zomzog@gmail.com>
2023-11-12 21:40:54 +0100

    init ++

--- init.sh
+++ init.sh
@@ -9,30 +9,52 @@ if [[ -z $name ]]; then
   exit 0
 fi
 
-path=$PWD/$name
+path=$PWD/../$name
+
+slides=$path/slides
 
 if [[ -d $path ]]; then
   echo "prez already exist"
   exit 0
 fi
 
-mkdir -p $path/slides/images
+echo "Create folders"
+mkdir -p $slides/images
 
-echo ":source-highlighter: highlightjs" > $path/slides/index.adoc
-echo ":revealjs_theme: black" >> $path/slides/index.adoc
-echo ":revealjs_progress: true" >> $path/slides/index.adoc
-echo ":revealjs_slideNumber: true" >> $path/slides/index.adoc
-echo ":revealjs_history: true" >> $path/slides/index.adoc
-echo ":revealjs_showNotes: false" >> $path/slides/index.adoc
-echo ":revealjs_width: 1080" >> $path/slides/index.adoc
-echo ":imagesdir: images" >> $path/slides/index.adoc
-echo ":customcss: css/custom.css" >> $path/slides/index.adoc
-echo ":docinfo: private" >> $path/slides/index.adoc
-echo "" >> $path/slides/index.adoc
-echo "== $name" >> $path/slides/index.adoc
+echo "Init index.adoc"
 
-ln -srf $PWD/_slides/* $path/slides/
-ln -srf $PWD/_slides/images/* $path/slides/images/
+echo ":source-highlighter: highlightjs" > $slides/index.adoc
+echo ":revealjs_theme: black" >> $slides/index.adoc
+echo ":revealjs_progress: true" >> $slides/index.adoc
+echo ":revealjs_slideNumber: true" >> $slides/index.adoc
+echo ":revealjs_history: true" >> $slides/index.adoc
+echo ":revealjs_showNotes: false" >> $slides/index.adoc
+echo ":revealjs_width: 1080" >> $slides/index.adoc
+echo ":imagesdir: images" >> $slides/index.adoc
+echo ":customcss: css/custom.css" >> $slides/index.adoc
+echo ":docinfo: private" >> $slides/index.adoc
+echo "" >> $slides/index.adoc
+echo "## $name" >> $slides/index.adoc
 
-ln -srf $PWD/_tooling/* $path
-ln -srf $PWD/_tooling/.nojekyll $path/.nojekyll
+echo "Add submodule"
+cd $path
+git init
+
+git submodule add git@github.com:Zomzog/prez-framework.git
+
+echo "Create links"
+ln -srf prez-framework/_slides/* $slides/
+ln -srf prez-framework/_slides/images/* $slides/images/
+
+ln -srf prez-framework/_tooling/* $path
+ln -srf prez-framework/_tooling/.nojekyll $path/.nojekyll
+
+ln -srf prez-framework/_github $path/.github
+cp prez-framework/.gitignore $path/.gitignore
+
+echo "First build"
+npm install
+
+echo "First commit"
+git add .
+git commit -m "First commit"
