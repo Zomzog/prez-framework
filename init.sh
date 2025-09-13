feb82b12a4ac6ec06b870ff5a37dac6764eea300
Thibault Duperron <zomzog@gmail.com>
2025-09-13 18:05:30 +0200

    init slidev (#14)

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ echo "Init new presentation"
 name=$1
 
 if [[ -z $name ]]; then
-  echo "name is missing"
-  exit 0
+    echo "name is missing"
+    exit 0
 fi
 
 path=$PWD/../$name
@@ -14,27 +14,20 @@ path=$PWD/../$name
 slides=$path/slides
 
 if [[ -d $path ]]; then
-  echo "prez already exist"
-  exit 0
+    echo "prez already exist"
+    exit 1
 fi
 
 echo "Create folders"
 mkdir -p $slides/images
 
-echo "Init index.adoc"
-
-echo ":source-highlighter: highlightjs" > $slides/index.adoc
-echo ":revealjs_theme: black" >> $slides/index.adoc
-echo ":revealjs_progress: true" >> $slides/index.adoc
-echo ":revealjs_slideNumber: true" >> $slides/index.adoc
-echo ":revealjs_history: true" >> $slides/index.adoc
-echo ":revealjs_showNotes: false" >> $slides/index.adoc
-echo ":revealjs_width: 1080" >> $slides/index.adoc
-echo ":imagesdir: images" >> $slides/index.adoc
-echo ":customcss: css/custom.css" >> $slides/index.adoc
-echo ":docinfo: private" >> $slides/index.adoc
-echo "" >> $slides/index.adoc
-echo "## $name" >> $slides/index.adoc
+if [[ -d $slides/slides.md ]]; then
+    echo "slides.md already exist"
+else
+    echo "Init slides.md"
+    cp _slides/slides.md $slides/slides.md
+    sed -i -e "s/theTitle/$name/g" "$slides/slides.md"
+fi
 
 echo "Add submodule"
 cd $path
@@ -42,17 +35,20 @@ git init
 git submodule add git@github.com:Zomzog/prez-framework.git
 
 echo "Create links"
-ln -srf prez-framework/_slides/* $slides/
+ln -srf prez-framework/_slides/.* $slides/
+ln -srf prez-framework/_slides/*.json $slides/
+ln -srf prez-framework/_slides/*.toml $slides/
 ln -srf prez-framework/_slides/images/* $slides/images/
 
-ln -srf prez-framework/_tooling/* $path
+ln -srf prez-framework/_tooling/* $slides
 ln -srf prez-framework/_tooling/.nojekyll $path/.nojekyll
 
 ln -srf prez-framework/_github $path/.github
 cp prez-framework/.gitignore $path/.gitignore
 
 echo "First build"
-npm install
+cd $slides
+pnpm install
 
 echo "First commit"
 git add .
